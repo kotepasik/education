7b915a6bfe45d14f5cdd7da3d63e1f168ec9764f
kotepasik <kotepasik@gmail.com>
2022-07-08 22:53:33 +0400

    Вывести квадрат числа

---
+++ seminar001/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Вывести квадрат числа
+// Число целое
+// Число генерируем рандомно
+// [0, 100]
+int n = new Random().Next(0, 100); 
+int m = n * n;
+//Console.WriteLine(n);
+//Console.WriteLine(m);
+Console.WriteLine($"{n}*{n}={m}");
+
+
