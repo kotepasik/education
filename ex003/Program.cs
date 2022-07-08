17ddfe2bc9e51cabf40151b16ba12fe738dc1953
kotepasik <kotepasik@gmail.com>
2022-07-08 15:09:17 +0400

    ex003 Сложение случайных чисел

--- ex003/Program.cs
+++ ex003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 5;
-int numberB = 65;
+﻿int numberA = new Random().Next(1, 10); // первое лучайное число от 1 до 9
+Console.WriteLine(numberA); // вывод 1-го числа на консоль с переводом строки
+int numberB = new Random().Next(1, 10); // второе лучайное число от 1 до 9
+Console.WriteLine(numberB); // вывод 2-го числа на консоль с переводом строки
 int result = numberA + numberB;
 Console.WriteLine(result);
 
